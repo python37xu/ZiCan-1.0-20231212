0c23eb10378e16347dfad998a4aa7e5db97e6c38
python37xu <15776461578@163.com>
2023-12-15 14:38:30 +0800

    新增资产模块

--- xinziweb_v1_0/mysite/settings.py
+++ xinziweb_v1_0/mysite/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'mysite.employee',
-    'mysite.myuserapp'
+    'mysite.myuserapp',
+    'mysite.asset'
 ]
 
 MIDDLEWARE = [
